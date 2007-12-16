7380e3c814d0fad9ef018915ae2377191ccfaa1a
JohnAlbin <virtually.johnalbin@gmail.com>
2007-12-16 10:47:16 +0000

    Added sample drupal search form override template file.

---
+++ search-theme-form.tpl.php
@@ -0,0 +1,5 @@
+<label for="search_theme_form_keys">Custom Search</label>
+<input type="text" maxlength="128" name="search_theme_form_keys" id="edit-search_theme_form_keys"  size="25" value="" title="Enter the terms you wish to search for." class="form-text" />
+<input type="submit" name="op" value="Search"  />
+<input type="hidden" name="form_id" id="edit-search-theme-form" value="search_theme_form" />
+<input type="hidden" name="form_token" id="a-unique-id" value="<?php print drupal_get_token('search_theme_form'); ?>" />
