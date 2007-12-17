60ec951ace46fd1a1f00a186d3b75bc12e2ca9f7
JohnAlbin <virtually.johnalbin@gmail.com>
2007-12-17 12:07:57 +0000

    Synced search-theme-form with standard Drupal search form.

--- search-theme-form.tpl.php
+++ search-theme-form.tpl.php
@@ -1,5 +1,8 @@
-<label for="search_theme_form_keys">Custom Search</label>
-<input type="text" maxlength="128" name="search_theme_form_keys" id="edit-search_theme_form_keys"  size="25" value="" title="Enter the terms you wish to search for." class="form-text" />
-<input type="submit" name="op" value="Search"  />
-<input type="hidden" name="form_id" id="edit-search-theme-form" value="search_theme_form" />
-<input type="hidden" name="form_token" id="a-unique-id" value="<?php print drupal_get_token('search_theme_form'); ?>" />
+<div id="search" class="container-inline">
+  <div class="form-item">
+    <input type="text" maxlength="128" name="search_theme_form_keys" id="edit-search-theme-form-keys"  size="15" value="" title="Enter the terms you wish to search for." class="form-text" />
+  </div>
+  <input type="submit" name="op" id="edit-submit" value="Search"  class="form-submit" />
+  <input type="hidden" name="form_token" id="edit-search-theme-form-form-token" value="<?php print drupal_get_token('search_theme_form'); ?>"  />
+  <input type="hidden" name="form_id" id="edit-search-theme-form" value="search_theme_form"  />
+</div>
